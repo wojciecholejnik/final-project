0be988c62b3ce949c923f4ba11ded21c2d24d979
Wojciech Olejnik <wojciecholejnik5@gmail.com>
2021-02-23 18:41:16 +0100

    init project files and add some sample data

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { createMuiTheme, StylesProvider, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+import { store } from './redux/store';
+
+import { MainLayout } from './components/layout/MainLayout/MainLayout';
+import { Homepage } from './components/views/Homepage/Homepage';
+import { NotFound } from './components/views/NotFound/NotFound';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2B4C6F' },
+  },
+});
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <MainLayout>
+            <Switch>
+              <Route exact path='/' component={Homepage} />
+              <Route path='*' component={NotFound} />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
+    </BrowserRouter>
+  </Provider>
+);
+
+export { App };
---
+++ src/redux/initialState.js
@@ -0,0 +1,50 @@
+export const initialState = {
+  products: {
+    data: [
+      {
+        _id:'1',
+        title: 'Mikrofon 1',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus non tellus et eleifend. Fusce placerat diam ultricies, pharetra diam nec, pellentesque velit. Maecenas rhoncus ullamcorper tellus, vel euismod justo fermentum sit amet. Nulla varius eleifend dolor, eu iaculis orci pellentesque eget. Nam cursus neque leo, vitae gravida tortor facilisis.',
+        price: 100,
+        img: 'mikrofon1.jpg',
+        type: 'stage',
+      },
+      {
+        _id:'2',
+        title: 'Mikrofon 2',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus non tellus et eleifend. Fusce placerat diam ultricies, pharetra diam nec, pellentesque velit. Maecenas rhoncus ullamcorper tellus, vel euismod justo fermentum sit amet. Nulla varius eleifend dolor, eu iaculis orci pellentesque eget. Nam cursus neque leo, vitae gravida tortor facilisis.',
+        price: 150,
+        img: 'mikrofon2.jpg',
+        type: 'stage',
+      },
+      {
+        _id:'3',
+        title: 'Mikrofon 3',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus non tellus et eleifend. Fusce placerat diam ultricies, pharetra diam nec, pellentesque velit. Maecenas rhoncus ullamcorper tellus, vel euismod justo fermentum sit amet. Nulla varius eleifend dolor, eu iaculis orci pellentesque eget. Nam cursus neque leo, vitae gravida tortor facilisis.',
+        price: 200,
+        img: 'mikrofon3.jpg',
+        type: 'studio',
+      },
+      {
+        _id:'4',
+        title: 'Mikrofon 4',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus non tellus et eleifend. Fusce placerat diam ultricies, pharetra diam nec, pellentesque velit. Maecenas rhoncus ullamcorper tellus, vel euismod justo fermentum sit amet. Nulla varius eleifend dolor, eu iaculis orci pellentesque eget. Nam cursus neque leo, vitae gravida tortor facilisis.',
+        price: 250,
+        img: 'mikrofon4.jpg',
+        type: 'studio',
+      },
+      {
+        _id:'5',
+        title: 'Mikrofon 5',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus non tellus et eleifend. Fusce placerat diam ultricies, pharetra diam nec, pellentesque velit. Maecenas rhoncus ullamcorper tellus, vel euismod justo fermentum sit amet. Nulla varius eleifend dolor, eu iaculis orci pellentesque eget. Nam cursus neque leo, vitae gravida tortor facilisis.',
+        price: 300,
+        img: 'mikrofon5.jpg',
+        type: 'stage',
+      }
+    ],
+    loading: {
+      active: false,
+      error: false,
+    },
+  },
+};
