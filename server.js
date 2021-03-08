71cf6252935b489f0fbbeb23015d120a3b704285
Wojtek <68703185+wojciecholejnik@users.noreply.github.com>
2021-03-08 22:21:42 +0100

    edit server endpoint for bulded front

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/api', cakesRoutes);
 app.use('/api', cupcakesRoutes);
 app.use('/api', ordersRoutes);
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
